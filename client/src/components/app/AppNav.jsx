4a7ee0524743a342160eb9f9078bece51bc84eec
StevenJBurns <stevenjburns@msn.com>
2018-12-05 16:55:12 -0700

    Added material-ui buttons with react-router-dom Link elements to AppNav --> works well

--- client/src/components/app/AppNav.jsx
+++ client/src/components/app/AppNav.jsx
@@ -1,10 +1,21 @@
 /* React, Redux and Router Imports */ 
 import React from "react";
+import { Link } from "react-router-dom";
+
+/* external dependencies */
+import Button from "@material-ui/core/Button";
 
 
 const AppNav = (props) => {
   return (
-    <nav></nav>
+    <nav>
+      <Button component={Link} to="/">HOME</Button>
+      <Button component={Link} to="/services">SERVICES</Button>
+      <Button component={Link} to="/contact">CONTACT</Button>
+      <Button component={Link} to="/about">ABOUT</Button>
+      <Button component={Link} to="/dashboard">DASHBOARD</Button>
+      <Button component={Link} to="/admin">ADMIN</Button>
+    </nav>
   );
 };
 
