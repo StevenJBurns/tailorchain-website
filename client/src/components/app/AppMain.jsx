174331462023e99cbe3ade85708dd0da7eca7449
StevenJBurns <stevenjburns@msn.com>
2018-12-05 09:34:52 -0700

    Created boilerplate AppMain.jsx component

---
+++ client/src/components/app/AppMain.jsx
@@ -0,0 +1,14 @@
+/* React, Redux and Router Imports */ 
+import React from "react";
+import { Switch } from "react-router";
+
+
+const AppMain = (props) => {
+  return (
+    <Switch>
+
+    </Switch>
+  );
+};
+
+export default AppMain;
