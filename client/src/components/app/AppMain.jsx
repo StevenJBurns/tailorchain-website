f92b61ca346b15a9c6a0e52a1bb391f30b4f18eb
StevenJBurns <stevenjburns@msn.com>
2018-12-05 16:35:19 -0700

    Re-added react-router Switch and related routes to AppMain... works manually but app needs button navigation

--- client/src/components/app/AppMain.jsx
+++ client/src/components/app/AppMain.jsx
@@ -1,15 +1,28 @@
 /* React, Redux and Router Imports */ 
 import React from "react";
-import { Switch } from "react-router";
+import { Switch, Route } from "react-router";
 
 /* Local Dependencies */
+import PageHome from "../pages/PageHome.jsx";
+import PageServices from "../pages/PageServices.jsx";
+import PageContact from "../pages/PageContact.jsx";
+import PageAbout from "../pages/PageAbout.jsx";
+import PageDashboard from "../pages/PageDashboard.jsx";
+import PageAdmin from "../pages/PageAdmin.jsx"; 
 import "./App.css";
 
 
 const AppMain = (props) => {
   return (
     <main>
-
+      <Switch>
+        <Route exact path="/" component={PageHome} />
+        <Route path="/services" component={PageServices} />
+        <Route path="/contact" component={PageContact} />
+        <Route path="/about" component={PageAbout} />
+        <Route path="/dashboard" component={PageDashboard} />
+        <Route path="/admin" component={PageAdmin} />
+      </Switch>
     </main>
   );
 };
