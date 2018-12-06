512d0375068b50a7fb200c077f5cc95b1a9d45e1
StevenJBurns <stevenjburns@msn.com>
2018-12-05 19:51:22 -0700

    Added login and log out Button components from mater-ui into AppHeader... still needs a boolean state toggle

--- client/src/components/app/AppHeader.jsx
+++ client/src/components/app/AppHeader.jsx
@@ -1,11 +1,16 @@
 /* React, Redux and Router Imports */ 
 import React from "react";
 
+/* External Dependencies */
+import Button from "@material-ui/core/Button";
+
 
 const AppHeader = (props) => {
   return (
     <header>
       <h1>TailorChain</h1>
+      <Button>LOGIN</Button>
+      <Button>LOG OUT</Button>
     </header>
   );
 };
